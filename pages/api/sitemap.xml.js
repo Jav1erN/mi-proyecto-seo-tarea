e2f889351fdfecbe1737cf9724f996fc5b8bf040
Javier Neira <javier.neira@tecsup.edu.pe>
2025-11-27 22:43:53 -0500

    Add static-compatible sitemap.xml

---
+++ pages/api/sitemap.xml.js
@@ -0,0 +1,25 @@
+export function getServerSideProps({ res }) {
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+      <loc>https://mi-proyecto-seo-tarea.vercel.app/</loc>
+    </url>
+    <url>
+      <loc>https://mi-proyecto-seo-tarea.vercel.app/blog</loc>
+    </url>
+    <url>
+      <loc>https://mi-proyecto-seo-tarea.vercel.app/contacto</loc>
+    </url>
+  </urlset>
+  `;
+
+  res.setHeader("Content-Type", "application/xml");
+  res.write(sitemap);
+  res.end();
+
+  return { props: {} };
+}
+
+export default function SiteMap() {
+  return null;
+}
